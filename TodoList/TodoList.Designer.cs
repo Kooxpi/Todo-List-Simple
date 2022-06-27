868bf299518de6da55cd7894901093df8a1967a2
Kooxpi <30616667+Kooxpi@users.noreply.github.com>
2022-06-27 15:17:40 +0300

    Initial commit
    Create TodoList.jpg

---
+++ TodoList/TodoList.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class TodoList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TodoListBox = new System.Windows.Forms.CheckedListBox();
+            this.AddBtn = new System.Windows.Forms.Button();
+            this.DelBtn = new System.Windows.Forms.Button();
+            this.EditBtn = new System.Windows.Forms.Button();
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // TodoListBox
+            // 
+            this.TodoListBox.FormattingEnabled = true;
+            this.TodoListBox.HorizontalScrollbar = true;
+            this.TodoListBox.Location = new System.Drawing.Point(12, 12);
+            this.TodoListBox.Name = "TodoListBox";
+            this.TodoListBox.Size = new System.Drawing.Size(239, 349);
+            this.TodoListBox.TabIndex = 0;
+            this.TodoListBox.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.TodoListBox_ItemCheck);
+            // 
+            // AddBtn
+            // 
+            this.AddBtn.Location = new System.Drawing.Point(12, 367);
+            this.AddBtn.Name = "AddBtn";
+            this.AddBtn.Size = new System.Drawing.Size(75, 23);
+            this.AddBtn.TabIndex = 1;
+            this.AddBtn.Text = "Add";
+            this.AddBtn.UseVisualStyleBackColor = true;
+            this.AddBtn.Click += new System.EventHandler(this.AddBtn_Click);
+            // 
+            // DelBtn
+            // 
+            this.DelBtn.Location = new System.Drawing.Point(94, 367);
+            this.DelBtn.Name = "DelBtn";
+            this.DelBtn.Size = new System.Drawing.Size(75, 23);
+            this.DelBtn.TabIndex = 2;
+            this.DelBtn.Text = "Delete";
+            this.DelBtn.UseVisualStyleBackColor = true;
+            this.DelBtn.Click += new System.EventHandler(this.DelBtn_Click);
+            // 
+            // EditBtn
+            // 
+            this.EditBtn.Location = new System.Drawing.Point(176, 367);
+            this.EditBtn.Name = "EditBtn";
+            this.EditBtn.Size = new System.Drawing.Size(75, 23);
+            this.EditBtn.TabIndex = 3;
+            this.EditBtn.Text = "Edit";
+            this.EditBtn.UseVisualStyleBackColor = true;
+            this.EditBtn.Click += new System.EventHandler(this.EditBtn_Click);
+            // 
+            // inputBox
+            // 
+            this.inputBox.Location = new System.Drawing.Point(13, 397);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(238, 20);
+            this.inputBox.TabIndex = 4;
+            // 
+            // TodoList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(263, 436);
+            this.Controls.Add(this.inputBox);
+            this.Controls.Add(this.EditBtn);
+            this.Controls.Add(this.DelBtn);
+            this.Controls.Add(this.AddBtn);
+            this.Controls.Add(this.TodoListBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "TodoList";
+            this.Text = "Todo List";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox TodoListBox;
+        private System.Windows.Forms.Button AddBtn;
+        private System.Windows.Forms.Button DelBtn;
+        private System.Windows.Forms.Button EditBtn;
+        private System.Windows.Forms.TextBox inputBox;
+    }
+}
+
---
+++ TodoList/TodoList.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class TodoList : Form
+    {
+        public TodoList()
+        {
+            InitializeComponent();
+        }
+        String filePath = @"todo.txt";
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (System.IO.File.Exists(filePath))
+                TodoListBox.Items.AddRange(System.IO.File.ReadAllLines(filePath));
+        }
+
+        private void AddBtn_Click(object sender, EventArgs e)
+        {
+            TodoListBox.Items.Clear();
+            File.AppendAllText(@"todo.txt", inputBox.Text + Environment.NewLine);
+            inputBox.Text = "";
+            TodoListBox.Items.AddRange(System.IO.File.ReadAllLines(filePath));
+        }
+
+        private void DelBtn_Click(object sender, EventArgs e)
+        {
+            string text = File.ReadAllText(filePath);
+            text = text.Replace(TodoListBox.Text, "");
+            File.WriteAllText(filePath, text);
+            File.WriteAllLines(filePath, File.ReadAllLines(filePath).Where(l => !string.IsNullOrWhiteSpace(l)));
+            TodoListBox.Items.Clear();
+            TodoListBox.Items.AddRange(System.IO.File.ReadAllLines(filePath));
+            inputBox.Text = "";
+        }
+
+        private void EditBtn_Click(object sender, EventArgs e)
+        {
+            string text = File.ReadAllText(filePath);
+            text = text.Replace(TodoListBox.Text, inputBox.Text);
+            if(inputBox.Text.Trim() == "")
+            {
+                MessageBox.Show("Entry cannot be blank!");
+            }else { 
+                File.WriteAllText(filePath, text);
+                TodoListBox.Items.Clear();
+                TodoListBox.Items.AddRange(System.IO.File.ReadAllLines(filePath));
+                inputBox.Text = "";
+            }
+        }
+
+        private void TodoListBox_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            
+            int sCount = TodoListBox.CheckedItems.Count;
+            if (e.NewValue == CheckState.Checked) { ++sCount; }
+            if (e.NewValue == CheckState.Unchecked) { --sCount; }
+            //MessageBox.Show(sCount.ToString());
+            if (sCount != 1) { inputBox.Text = ""; } else { inputBox.Text = TodoListBox.Text; }
+        }
+    }
+
+
+}
